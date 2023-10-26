296a18778cb3489a0379bcaa40b5ea8050e7ebb2
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2023-10-26 15:04:42 -0700

    Install 'zip' on Windows

--- setup.bat
+++ setup.bat
@@ -4,9 +4,18 @@ setlocal EnableDelayedExpansion
 winget install -e --id Microsoft.DotNet.Runtime.6
 winget install -e --id Microsoft.DotNet.Runtime.7
 winget install -e --id GnuWin32.Make
+
+:: Set execution policy so that we can install Scoop
 gsudo pwsh -NoProfile  -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser"
 pwsh -NoProfile -Command "irm get.scoop.sh | iex"
+
+:: Install shellcheck primarily for shell script linting in VSCode
 scoop install shellcheck
+
+:: Zip is required when generating packages
+::  tools\make-firefox.sh: line 34: zip: command not found
+::  make: *** [dist/build/uBlock0.firefox] Error 127
+scoop install zip
 
 if exist "%~dp0.gdn\e\semmle.gdnconfig" goto:$SkipGuardian
 if not exist "%APPDATA%\Code\User\globalStorage\devprod.vulnerability-extension\guardian\cli\guardian.cmd" goto:$SkipGuardian
